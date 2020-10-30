b8c2398e3593735c9088d95ce239e011c3684f10
Dmitry Trifonov <D.Trifonov@external.t-systems.com>
2020-10-30 10:11:38 +0300

    Long work added

--- src/main/java/com/tsystems/mb2b/openshift/graceful_shutdown/WorkController.java
+++ src/main/java/com/tsystems/mb2b/openshift/graceful_shutdown/WorkController.java
@@ -5,13 +5,19 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class WorkController {
 
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/dowork")
-    public WorkResult doWork() {
+    public WorkResult doWork() throws InterruptedException {
+        log.info("dowork enter");
+        Thread.sleep(60_000);
+        log.info("dowork exit");
         return new WorkResult(counter.incrementAndGet());
     }
 }
