c13b665340e7c6911cb318dfb65535399f9888e1
Dmitry Trifonov <D.Trifonov@external.t-systems.com>
2020-10-30 11:24:07 +0300

    tomcat graceful shutdown attempt

--- src/main/java/com/tsystems/mb2b/openshift/graceful_shutdown/GracefulShutdownApplication.java
+++ src/main/java/com/tsystems/mb2b/openshift/graceful_shutdown/GracefulShutdownApplication.java
@@ -2,6 +2,9 @@ package com.tsystems.mb2b.openshift.graceful_shutdown;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GracefulShutdownApplication {
@@ -10,4 +13,15 @@ public class GracefulShutdownApplication {
         SpringApplication.run(GracefulShutdownApplication.class, args);
     }
 
+    @Bean
+    public TomcatGracefulShutdown gracefulShutdown() {
+        return new TomcatGracefulShutdown();
+    }
+
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory(final TomcatGracefulShutdown gracefulShutdown) {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(gracefulShutdown);
+        return factory;
+    }
 }
